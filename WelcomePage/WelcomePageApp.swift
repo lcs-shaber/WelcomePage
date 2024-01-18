8cdc47fff0b72698f161af99a0084d65d4533ee8
Sophie Haber <SHaber@lcs.on.ca>
2024-01-17 19:07:45 -0500

    Initial Commit

---
+++ WelcomePage/WelcomePageApp.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomePageApp.swift
+//  WelcomePage
+//
+//  Created by Sophie Haber on 17.01.24.
+//
+
+import SwiftUI
+
+@main
+struct WelcomePageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
